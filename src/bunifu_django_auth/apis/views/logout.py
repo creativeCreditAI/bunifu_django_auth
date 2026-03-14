071254a2b88b3cf05c849e6be7b6bc8f31a7c1eb
dlion4 <dantemilimo@gmail.com>
2026-03-15 01:22:57 +0300

    updated the 'cannot guess the serializer with the logout view'

--- src/bunifu_django_auth/apis/views/logout.py
+++ src/bunifu_django_auth/apis/views/logout.py
@@ -1,16 +1,18 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from ..serializers.logout import LogoutSerializer
+from rest_framework.generics import GenericAPIView
 
-
-class LogoutView(APIView):
+class LogoutView(GenericAPIView):
+    serializer_class = LogoutSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
-        serializer = LogoutSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         return Response(
-            {"detail": "Successfully logged out."}, status=status.HTTP_200_OK
+            {"detail": "Successfully logged out."},
+            status=status.HTTP_200_OK,
         )
